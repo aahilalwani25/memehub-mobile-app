cea3e2de1f01789885aa7995b88938750794e71d
aahilalwani25 <alwani.aahil25@gmail.com>
2023-10-05 14:22:43 +0500

    updated

---
+++ lib/Views/signup_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('')
+            
+          )
+        ),
+      ),
+    );
+  }
+}
