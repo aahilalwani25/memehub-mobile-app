eed835f3b0d1e3f86692edc35a3b3bb6d6b36090
User <filzawasim822@gmail.com>
2023-10-08 16:42:10 +0500

    abc

--- lib/Bloc/signup bloc/signup_event.dart
+++ lib/Bloc/signup bloc/signup_event.dart
@@ -1,4 +1,29 @@
 part of 'signup_bloc.dart';
 
-@immutable
-sealed class SignupEvent {}
+sealed class SignupEvent extends Equatable {
+  const SignupEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AcceptthetermsEvent extends SignupEvent{
+  bool agree;
+
+  AcceptthetermsEvent({required this.agree});
+
+@override
+  List<Object> get props => [agree];
+ 
+}
+
+class RegisterEvent extends SignupEvent{
+  String username,email ,password ;
+  bool AcceptthetermsEvent;
+  RegisterEvent ({ required this.username, required this.email, required this.password,required this.AcceptthetermsEvent });
+
+  @override
+  List<Object> get props => [username,email,password,AcceptthetermsEvent];
+}
+
+class LoadingEvent extends SignupEvent{}
--- lib/Bloc/signup bloc/signup_state.dart
+++ lib/Bloc/signup bloc/signup_state.dart
@@ -1,6 +1,35 @@
 part of 'signup_bloc.dart';
 
-@immutable
-sealed class SignupState {}
+sealed class SignupState extends Equatable {
+  const SignupState();
+  
+  @override
+  List<Object> get props => [];
+}
 
 final class SignupInitial extends SignupState {}
+
+class AcceptthetermsState extends SignupState{
+
+  bool agree;
+  AcceptthetermsState({required this.agree});
+}
+
+class SignupSuccessfulState extends SignupState{
+
+  String success;
+
+  SignupSuccessfulState({required this.success});
+
+  @override
+  List<Object> get props => [success];
+}
+
+class SignupUnsuccessfulState extends SignupState{
+  String error;
+
+  SignupUnsuccessfulState({required this.error});
+
+  @override
+  List<Object> get props => [error];
+}
