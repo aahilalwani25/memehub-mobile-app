ee60a4510285565af69b3d1c7d3da33d22de16ba
User <filzawasim822@gmail.com>
2023-10-14 14:28:45 +0500

    signup complete

--- lib/Bloc/signup bloc/signup_bloc.dart
+++ lib/Bloc/signup bloc/signup_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
 
 part 'signup_event.dart';
 part 'signup_state.dart';
@@ -12,7 +13,36 @@ class SignupBloc extends Bloc<SignupEvent, SignupState> {
 
     on<AcceptthetermsEvent>((event, emit) => emit(AcceptTheTermsState(agree: event.agree)));
 
-  on<RegisterEvent>((event, emit){
-  });
+  
+    on<RegisterButtonPressedEvent>((event, emit) async{
+      //login logic here
+      //http://127.0.0.1:8000/api/user/register
+      emit(SignupLoadingState());
+      final response= await http.post(
+        Uri.parse('http://192.168.0.106:8000/api/user/login'),
+        headers: {
+          'Accept': 'application/json'
+        },
+        body: {
+          'email': event.email,
+          'name': event.name,
+          'password': event.password,
+          'confirm password' : event.password_confirmation,
+
+        },
+      );
+
+      //if user successfully registers in
+      if(response.statusCode==200){
+        emit(SignupSuccessfulState(success: 'Registered Successfully'));
+      }else{
+        emit(SignupUnsuccessfulState(error: 'Either name  is wrong'));
+      }
+
+
+    });
   }
+
 }
+
+  
