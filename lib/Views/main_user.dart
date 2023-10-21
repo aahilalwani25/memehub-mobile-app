e320859cc5a92a51f70a51453d6ae8d75096581a
User <filzawasim822@gmail.com>
2023-10-21 22:36:42 +0500

    changed tab bar from dashboard to main_user

--- lib/Views/main_user.dart
+++ lib/Views/main_user.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
 
+import '../tab/home_tab.dart';
+import '../tab/settings_tab.dart';
+import '../tab/trending_tab.dart';
+
 class homescreen extends StatelessWidget{
   const homescreen({Key? key}) : super(key: key);
   @override
-  Widget  build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text("Homescreen"),
-      ),
+  Widget build(BuildContext context){
+  return DefaultTabController(
+
+    length: 3,
+    child: Scaffold(
+     appBar: AppBar(
+      title: const Text('HOME SCREEN'),
+     ),
+     body: const Column(
+      children: [
+        TabBar(tabs:[
+          Tab(
+            icon:  Icon(
+              Icons.home,
+              color: Colors.black,
+            ),
+          ),
+          Tab(
+            icon:  Icon(
+              Icons.arrow_upward_outlined,
+              color: Colors.black,
+            ),
+          ),
+          Tab(
+            icon:  Icon(
+              Icons.settings,
+              color: Colors.black,
+            ),
+          )
+        ] 
+        ),
+      Expanded(
+        child: TabBarView(
+          children:[
+            //1tab
+            home(),
+
+            //2 tab
+            Trending(),
+            //3tab
+            settings(),
+          ]
       
-      );
-  }
+          ),
+          ),
+      ],
+     ),
+    ),
+    
+  );
+
+}
+
+
 }
